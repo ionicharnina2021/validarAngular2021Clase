c834658e2024d73690ab0aa096426b3079643b6c
programacion <>
2021-02-08 11:56:44 +0100

    que si

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,8 +12,23 @@ export class HomePage {
   nombre:string;
   constructor() {
     this.ejemploForm=new FormGroup({
-      nombreEntrada:new FormControl('',Validators.minLength(3))
+      nombreEntrada:new FormControl('',Validators.minLength(3)),
+      telefonoMovil:new FormControl('',Validators.pattern("\."))
+
     });
   }
-
+  validationMessages = {
+    'nameInput': {
+      'required': 'Username is required.',
+      'minlength': 'Username must be at least 5 characters long.',
+      'maxlength': 'Username cannot be more than 25 characters long.',
+      'pattern': 'Your username must contain only numbers and letters.',
+      'validUsername': 'Your username has already been taken.'
+    },
+    //mensajes para el año
+    'yearInput':{
+      'isOld':"Deben ser posteriores a 1900",
+      'pattern': 'el año no cumple el patron'
+    }
+  }
 }
